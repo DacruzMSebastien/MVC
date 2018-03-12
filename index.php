d3bfc3dcd2a3ff64d767cfcaf05c40a7c68bf2b5
dacruzmsebastien <dacruzmsebastien@gmail.com>
2018-03-12 11:38:27 +0100

    dir created

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Php minichat</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div>
+    <section>
+      <form action="" method="POST">
+      </form>
+    </section>
+  </div>
+</body>
+</html>
